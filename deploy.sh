663778d73e674c84a71cb1e1c9643def1a497348
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2025-10-26 09:04:10 -0500

    updates deploy.sh to support single deploys

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 dir='/Users/kevin/code/kevinkowalew/unknown/helm-values'
 files=( $(ls $dir) )
 
-for file in "${files[@]}"; do
-  deploy=$(echo $file | cut -d"." -f1)  
-  helm upgrade --install -n prod $deploy . --values=$dir/$file
-done
+if [ $# -eq 0 ]; then
+	for file in "${files[@]}"; do
+	  deploy=$(echo $file | cut -d"." -f1)  
+	  helm upgrade --install -n prod $deploy . --values=$dir/$file
+	done
+elif [ $# -lt 2 ]; then
+	for (( i=1; i<=$#; i++ )); do
+	  path="${dir}/${!i}.yaml"
+	  if [ -e $path ]; then
+		  helm upgrade --install -n prod ${!i} . --values=$path
+		  echo $path
+	  fi
+	done
+fi
